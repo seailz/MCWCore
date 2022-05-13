d65df2a0b61ba647ad87cbb3b9e5f50a99fffee6
IoyoCode <81972974+IoyoCode@users.noreply.github.com>
2022-05-13 22:19:17 +0100

    Added getting color code from string

--- src/main/java/live/mcwipeout/mcwcore/commands/team/sub/Create.java
+++ src/main/java/live/mcwipeout/mcwcore/commands/team/sub/Create.java
@@ -14,6 +14,34 @@ import org.bukkit.command.CommandSender;
 public class Create extends SubCommand {
     @Override
     public void onCommand(CommandSender sender, String[] args) {
-        new Team(args[0], ChatColor.RED);
+        new Team(args[0], getChatColorByCode(args[2]));
+    }
+
+    private ChatColor getChatColorByCode(String colorCode){
+        switch (colorCode){
+            case "b" : return ChatColor.AQUA;
+            case "0" : return ChatColor.BLACK;
+            case "9" : return ChatColor.BLUE;
+            case "l" : return ChatColor.BOLD;
+            case "3" : return ChatColor.DARK_AQUA;
+            case "1" : return ChatColor.DARK_BLUE;
+            case "8" : return ChatColor.DARK_GRAY;
+            case "2" : return ChatColor.DARK_GREEN;
+            case "5" : return ChatColor.DARK_PURPLE;
+            case "4" : return ChatColor.DARK_RED;
+            case "6" : return ChatColor.GOLD;
+            case "7" : return ChatColor.GRAY;
+            case "a" : return ChatColor.GREEN;
+            case "o" : return ChatColor.ITALIC;
+            case "d" : return ChatColor.LIGHT_PURPLE;
+            case "k" : return ChatColor.MAGIC;
+            case "c" : return ChatColor.RED;
+            case "r" : return ChatColor.RESET;
+            case "m" : return ChatColor.STRIKETHROUGH;
+            case "n" : return ChatColor.UNDERLINE;
+            case "f" : return ChatColor.WHITE;
+            case "e" : return ChatColor.YELLOW;
+            default: return ChatColor.WHITE;
+        }
     }
 }
